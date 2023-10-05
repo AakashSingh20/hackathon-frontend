0351a1e6b2de31ef21f55cf7249a6c0699a898fa
Aakash Singh <akashsingh20603@gmail.com>
2023-10-05 06:39:41 +0530

    completed without home

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import Context from "./context/MainContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Context>
     <App />
-  </React.StrictMode>
+  </Context>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
